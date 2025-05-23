4ee63a281b94b9701cbed5b8f722bc5dfe59e9f2
Niwa-Yume <julien.castro@protonmail.com>
2025-05-23 13:19:17 +0200

    test nouvelle lib ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,18 @@
 import DefaultLayout from "@/layouts/default";
 import UnicornStudioEmbed from "@/components/UnicornStudioEmbed.tsx";
+import { SplashCursor } from "@/components/ui/splash-cursor";
+import { Hero } from "@/components/ui/animated-hero";
 
 export default function IndexPage() {
   return (
     <DefaultLayout>
+      <SplashCursor />
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <UnicornStudioEmbed/>
       </section>
+      <div className="block">
+        <Hero />
+      </div>
     </DefaultLayout>
   );
 }
